1263a20eb570fff78b089078ced64be9780060e9
Will Brown <wtbrown10@gmail.com>
2020-07-28 15:37:32 -0400

    first and second npm

--- firstNpm/app.js
+++ firstNpm/app.js
@@ -1,1 +1,21 @@
-console.log('Mic check testing 123!')
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+
+const port = 3000
+
+// route handler
+app.get(
+    "/",
+    (req, res) => {
+        res.json({
+            message: "this is in an object!"
+        })
+
+    }
+)
+
+app.listen(port, () => {
+    console.log(`Server is now listening on port ${port}`)
+})

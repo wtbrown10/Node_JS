eff70c8d6feae137efb1a94376fc1a5dd1115cd2
will <wtbrown10@gmail.com>
2020-08-13 18:30:48 -0400

    tuesday

--- fourthNpm/app.js
+++ fourthNpm/app.js
@@ -15,10 +15,11 @@ const middle = require('./middleware/middleware')
 //routers
 const homeRouter = require('./routes/homeRouter')
 const userRouter = require('./routes/userRouter')
-const { request } = require('express')
+
 
 // constants
 const port = process.env.PORT || 3000;
+const URI = process.env.MONGO_URI
 
 //Middleware and Routes in use
 app.use(express.static('public')) // allows you to view js and css files in public folder
@@ -27,7 +28,24 @@ app.use(middle);
 app.use('/', homeRouter)
 app.use('/user', userRouter)
 
+if (typeof URI === 'string') {
 
+    const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+
+    // Connection to database
+    mongoose.connect(URI, mongoOptions, (err) => {
+
+        if (err) {
+            console.error(`\nError Connecting To MongoDB: ${err.message || err}\n`)
+        } else {
+            console.log('Server connected DB');
+        }
+    })
+
+
+} else {
+    console.error("Mongo URI missing or invalid, check node environment variables")
+}
 
 // Start server listening
 app.listen(port, function () {
